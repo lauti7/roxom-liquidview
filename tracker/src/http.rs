4aa27c78c06fd29c8f7cb7dad8b425b4616da67b
lauti7 <lautigbustos@gmail.com>
2026-02-08 12:19:23 -0300

    cors

--- tracker/src/http.rs
+++ tracker/src/http.rs
@@ -1,10 +1,15 @@
+use actix_cors::Cors;
+
 use crate::{api, liquidity_service::LiquidityService, pg::Database};
-use actix_web::{App, HttpServer, web};
+use actix_web::{web, App, HttpServer};
 
 pub fn create_http_server(server_addr: &str, db: &Database) -> actix_web::dev::Server {
     let liquidity_service = LiquidityService::new(db.clone());
     HttpServer::new(move || {
+        let cors = Cors::permissive();
+
         App::new()
+            .wrap(cors)
             .app_data(web::Data::new(liquidity_service.clone()))
             .configure(api::configure)
     })
